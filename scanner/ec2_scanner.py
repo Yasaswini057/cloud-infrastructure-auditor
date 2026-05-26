f157437b6b148c3c3742e3d8a6c6325fa9504c35
Hasini <24B11IT095@adityauniversity.in>
2026-05-26 15:03:01 +0530

    integrate cloudwatch cpu analysis into ec2 scanner

--- scanner/ec2_scanner.py
+++ scanner/ec2_scanner.py
@@ -5,14 +5,16 @@ from utils.logger import logger
 from utils.aws_regions import get_all_regions
 
 
+from aws.cloudwatch_utils import get_cpu_avg
+
+
 def scan_ec2_instances():
     """
-    Scan EC2 instances across multiple AWS regions
+    Scan EC2 instances across multiple AWS regions with CPU analysis
     """
 
     try:
         session = create_aws_session()
-
         regions = get_all_regions()
 
         instances_data = []
@@ -26,24 +28,36 @@ def scan_ec2_instances():
             response = ec2_client.describe_instances()
 
             for reservation in response["Reservations"]:
-
                 for instance in reservation["Instances"]:
 
+                    instance_id = instance.get("InstanceId")
+
+                    
+                    cpu = get_cpu_avg(instance_id)
+
+                    if cpu is None:
+                        status = "NO DATA"
+                    elif cpu < 5:
+                        status = "UNDERUTILIZED"
+                    else:
+                        status = "ACTIVE"
+
                     instance_info = {
-                        "InstanceId": instance.get("InstanceId"),
+                        "InstanceId": instance_id,
                         "State": instance.get("State", {}).get("Name"),
                         "Type": instance.get("InstanceType"),
                         "Region": region,
-                        "CpuUtilization": "3%"
+                        "CpuUtilization": cpu,
+                        "Status": status
                     }
 
                     instances_data.append(instance_info)
+
         print(f"EC2 Instances Scanned: {len(instances_data)}")
 
         return instances_data
 
     except (BotoCoreError, ClientError) as error:
-
         logger.error(f"EC2 scanning failed: {str(error)}")
 
     return []
