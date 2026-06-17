1d6889673bd0e08d82ff01fc2647db360b299928
Yasaswini <yasaswini5kk@gmail.com>
2026-06-17 21:00:39 +0530

    Testing completed

--- auth/config.py
+++ auth/config.py
@@ -5,4 +5,4 @@ load_dotenv()
 
 AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
-AWS_REGION = os.getenv("AWS_REGION")
+AWS_REGION = os.getenv("AWS_REGION", "ap-south-1")
